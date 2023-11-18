b1d8f94671b1d9db681fce71b1471f59b44b2383
JULIEDIANA237 <jj0938108@gmail.com>
2023-11-18 23:38:19 +0100

    Mes premières fonctionnalitées

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-const progressBars = document.querySelectorAll('.progress-bar');
-
-// Set progress bar values
-progressBars.forEach((progressBar, index) => {
-  const value = (index + 1) * 25;
-  progressBar.style.width = `${value}%`;
+document.getElementById("ajouter").addEventListener("click", function() {
+  var competence = prompt("Nom de la compétence :");
+  var pourcentage = prompt("Pourcentage :");
+  
+  if (competence && pourcentage) {
+    var skillHTML = '<div class="skill"><span class="competence">' + competence + '</span><div class="progress"><div class="progress-bar" style="width: ' + pourcentage + '%;"></div></div></div>';
+    document.querySelector(".skills").innerHTML += skillHTML;
+  }
 });
 
-function addSkill() {
-  var skillsContainer = document.getElementById('skills-container');
+document.getElementById("ajout").addEventListener("click", function() {
+  var diplome = prompt("Nom du diplôme :");
+  var ecole = prompt("École/Année :");
+  var matieres = prompt("Liste des matières (séparées par des virgules) :");
+  
+  if (diplome && ecole && matieres) {
+    var formationHTML = '<div class="diplome"><h6>' + diplome + '</h6><em>' + ecole + '</em><ul>';
+    var matiereArray = matieres.split(",");
+    
+    for (var i = 0; i < matiereArray.length; i++) {
+      formationHTML += '<li>' + matiereArray[i].trim() + '</li>';
+    }
+    
+    formationHTML += '</ul></div>';
+    
+    document.querySelector(".diplome").innerHTML += formationHTML;
+  }
+});
 
-  // Crée une nouvelle compétence
-  var skill = document.createElement('div');
-  skill.classList.add('skill');
-
-  // Demande à l'utilisateur le nom et le pourcentage de la compétence
-  var skillName = prompt('Nom de la compétence :');
-  var skillPercentage = prompt('Pourcentage de la compétence :');
-
-  // Crée le titre de la compétence
-  var skillTitle = document.createElement('h3');
-  skillTitle.textContent = skillName;
-
-  // Crée la barre de progression
-  var progressBar = document.createElement('div');
-  progressBar.classList.add('progress-bar');
-
-  // Crée la progression (en fonction du pourcentage entré)
-  var progress = document.createElement('div');
-  progress.classList.add('progress');
-  progress.style.width = skillPercentage + '%';
-
-  // Ajoute la progression à la barre de progression
-  progressBar.appendChild(progress);
-
-  // Ajoute le titre et la barre de progression à la compétence
-  skill.appendChild(skillTitle);
-  skill.appendChild(progressBar);
-
-  // Ajoute la compétence à la grille des compétences
-  skillsContainer.appendChild(skill);
-}
+document.getElementById("add").addEventListener("click", function() {
+  var experience = prompt("Nom de l'expérience :");
+  // var ecole = prompt("École/Année :");
+  var experiences = prompt("Liste des expériences (séparées par des virgules) :");
+  
+  if (experience  && matieres) {
+    var experienceHTML = '<div class="diplome"><h6>' + experience + '</h6><ul>';
+    var experienceArray = experiences.split(",");
+    
+    for (var i = 0; i < experienceArray.length; i++) {
+      experienceHTML += '<li>' + experienceArray[i].trim() + '</li>';
+    }
+    
+    experienceHTML += '</ul></div>';
+    
+    document.querySelector(".experience").innerHTML += experienceHTML;
+  }
+});
