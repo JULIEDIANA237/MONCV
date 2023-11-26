e12cb171b388eaa9b5f77e358a7eedaf7236d3b7
JULIEDIANA237 <dianajuliefontsa@gmail.com>
2023-11-26 15:15:59 +0100

    my first design pages using html css and js

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-document.getElementById("ajouter").addEventListener("click", function() {
-  var competence = prompt("Nom de la compétence :");
-  var pourcentage = prompt("Pourcentage :");
-  
-  if (competence && pourcentage) {
-    var skillHTML = '<div class="skill"><span class="competence">' + competence + '</span><div class="progress"><div class="progress-bar" style="width: ' + pourcentage + '%;"></div></div></div>';
-    document.querySelector(".skills").innerHTML += skillHTML;
-  }
+document.getElementById('downloadButton').addEventListener('click', function() {
+  var filename = 'mon-cv.pdf';
+
+  html2pdf().set({
+    margin: 10,
+    filename: filename,
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+  }).from(document.body).save();
 });
 
-document.getElementById("ajout").addEventListener("click", function() {
-  var diplome = prompt("Nom du diplôme :");
-  var ecole = prompt("École/Année :");
-  var matieres = prompt("Liste des matières (séparées par des virgules) :");
-  
-  if (diplome && ecole && matieres) {
-    var formationHTML = '<div class="diplome"><h6>' + diplome + '</h6><em>' + ecole + '</em><ul>';
-    var matiereArray = matieres.split(",");
-    
-    for (var i = 0; i < matiereArray.length; i++) {
-      formationHTML += '<li>' + matiereArray[i].trim() + '</li>';
-    }
-    
-    formationHTML += '</ul></div>';
-    
-    document.querySelector(".diplome").innerHTML += formationHTML;
-  }
-});
 
-document.getElementById("add").addEventListener("click", function() {
-  var experience = prompt("Nom de l'expérience :");
-  // var ecole = prompt("École/Année :");
-  var experiences = prompt("Liste des expériences (séparées par des virgules) :");
-  
-  if (experience  && matieres) {
-    var experienceHTML = '<div class="diplome"><h6>' + experience + '</h6><ul>';
-    var experienceArray = experiences.split(",");
-    
-    for (var i = 0; i < experienceArray.length; i++) {
-      experienceHTML += '<li>' + experienceArray[i].trim() + '</li>';
-    }
-    
-    experienceHTML += '</ul></div>';
-    
-    document.querySelector(".experience").innerHTML += experienceHTML;
-  }
-});
+
+
+
+
